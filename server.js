3ca795e735bea4683053db9b921b47a7ba29ec24
EmbraCraig <embracraig@gmail.com>
2020-03-02 21:43:30 +0000

    Added some layout defaults and bootstrap

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // configure view engine
 app.set('views', './views');
@@ -8,6 +9,15 @@ app.set('view engine', 'pug');
 //routes
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
+
+// serve /views/public as static resources
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static('/public', {
+    index: false, 
+    immutable: true, 
+    cacheControl: true,
+    maxAge: '30d'
+}));
 
 // export
 module.exports = app;
